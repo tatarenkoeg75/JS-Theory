d008a5ba3c9b36ba9d0bcd9a37ecd1e57ecbb271
Evgenii Tatarenko <tatrenkoeg75@gmail.com>
2024-08-06 23:21:48 +0700

    Перебираемые объекты

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 /**
- * https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+ * https://learn.javascript.ru/iterable
  */
 
-//? padStart()
+//? Array.from
 const string = "Test string";
 Array.from(string).map((latter, index) => console.log( `${''.padStart(index)}${latter}` ));
+
+//? 
+// const obj = {
+//   index: 0,
+//   // gap() { return ''.padStart(this.index++) }, 
+// }
+// const arr = Array.from(string, latter => `${''.padStart(obj.index++)} ${latter.toUpperCase()}`, obj )
+// arr.map( item =>  console.log(item));
+
+
+
+//? Slice for emoji
+// console.log( str.slice(1, 3) ); //!Not Work
+const slice = (str, start, end) => Array.from(str).slice(start, end).join('');
+let str = '𝒳😂𩷶';
+console.log( slice(str, 1, 3) );
